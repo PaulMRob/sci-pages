758eb45c4b93cc6f24edea0094e14d5b88aa9a16
Paul Robertson <bigwallpaul514@gmail.com>
2025-03-20 10:21:19 -0600

    styling for FacultyProfile.js

--- src/components/FacultyProfile.js
+++ src/components/FacultyProfile.js
@@ -2,7 +2,7 @@
 // components/FacultyProfile.js
 export default function FacultyProfile({ faculty }) {
     return (
-      <div className="max-w-4xl mx-auto p-8">
+      <div className="max-w-4xl mx-auto flex items-center gap-x-4 rounded-xl bg-white shadow-lg outline outline-white/5 p-8">
          <div className="flex justify-center mb-6">
             <img
             src={faculty.image}
